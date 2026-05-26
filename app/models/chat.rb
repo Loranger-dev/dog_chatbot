0d6f8bc30335972762fc90042d6876185c1e5f78
Rindrasoa Rasamoelina <rindramiangaly@gmail.com>
2026-05-26 16:39:36 +0200

    Ajout modèle

---
+++ app/models/chat.rb
@@ -0,0 +1,8 @@
+class Chat < ApplicationRecord
+  belongs_to :user
+  belongs_to :dog
+
+  has_many :messages, dependent: :destroy
+
+  validates :title, presence: true
+end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,10 @@
 class Dog < ApplicationRecord
+  belongs_to :user
+
+  has_many :chats, dependent: :destroy
 
   validates :name, presence: true
   validates :breed, presence: true
+  validates :personality, presence: true
+  validates :gender, presence: true
 end
