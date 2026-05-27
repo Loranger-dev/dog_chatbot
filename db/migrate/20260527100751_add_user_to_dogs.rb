da39cd79483130201afc39887458dd6c520ead7c
Remi Chaput <remi.chpt@gmail.com>
2026-05-27 12:12:15 +0200

    dog_userid

---
+++ db/migrate/20260527100751_add_user_to_dogs.rb
@@ -0,0 +1,5 @@
+class AddUserToDogs < ActiveRecord::Migration[8.1]
+  def change
+    add_reference :dogs, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_27_095410) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_27_100751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,7 +31,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_27_095410) do
     t.text "description"
     t.string "name"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.bigint "user_id_id"
+    t.index ["user_id"], name: "index_dogs_on_user_id"
     t.index ["user_id_id"], name: "index_dogs_on_user_id_id"
   end
 
@@ -58,5 +60,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_27_095410) do
 
   add_foreign_key "chats", "dogs"
   add_foreign_key "chats", "users"
+  add_foreign_key "dogs", "users"
   add_foreign_key "messages", "chats"
 end
